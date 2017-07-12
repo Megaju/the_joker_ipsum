2aa34b0b8cc83850a246755daddaa5b6b6fbf4d1
Megaju <megaju@kijewoku.fr>
2017-07-12 18:07:41 +0200

    valeurs par défaut dans le form

--- index.php
+++ index.php
@@ -6,9 +6,9 @@ require 'src/GenerateParagraph.php';
 
     <form action="" method="get">
         <label for="carac">Number of characters</label>
-        <input type="number" name="carac" id="carac" min="50" max="10000" value="50">
+        <input type="number" name="carac" id="carac" min="50" max="10000" value="<?php if(isset($_GET['carac'])){echo $_GET['carac'];}else{echo "250";}; ?>">
         <label for="carac">Number of paragraphs</label>
-        <input type="number" name="nb_p" id="nb_p" min="1" max="20" value="1">
+        <input type="number" name="nb_p" id="nb_p" min="1" max="20" value="<?php if(isset($_GET['nb_p'])){echo $_GET['nb_p'];}else{echo "1";}; ?>">
         <input type="submit" name="validate-paragraph" value="generate">
     </form>
 
