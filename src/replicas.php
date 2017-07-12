5d3549d5636750d2138ec62da5423c8ae9155147
Megaju <megaju@kijewoku.fr>
2017-07-12 20:38:03 +0200

    Répliques mises à jour ! ! !

--- src/replicas.php
+++ src/replicas.php
@@ -7,9 +7,25 @@
  */
 
 $replicas = array(
+    "To them you're a freak like me.",
     "Why so serious ?",
-    "It's simple. We kill the Batman.",
-    "Everything. Burns.",
+    "It's simpe. We kill the Batman.",
+    "The only sensible way to live in this world is without rules.",
+    "EVERTHING. BURNS.",
+    "I'm a man of simple tastes.",
+    "Do I really look like a man with a plane ?",
+    "Why should I have all the fun ?",
+    "Lets put a smile on that face.",
     "This is my town now.",
-    "The only sensible way to live in this world is without rules."
+    "I'm not a monster I'm just ahead of the curve to them.",
+    "Madness as you know is like.",
+    "Gravity all it takes is a little push.",
+    "How about a magic trik ?",
+    "I'm a agent of chaos.",
+    "ALARM",
+    "VAULT",
+    "TELLERS",
+    "GUARDS",
+    "HOSTAGES",
+    "GETAWAY"
 );
