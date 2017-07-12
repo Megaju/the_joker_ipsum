d5c7a6ff2cb2d378d56ab93b8d63f52a34c819a0
Megaju <megaju@kijewoku.fr>
2017-07-12 13:50:50 +0200

    plusieurs paragraphes

--- index.php
+++ index.php
@@ -1,44 +1,27 @@
 <?php
 
-/*
- * CREATION DE PARAGRAPH
- *
- * On veut un tableaux contenant les répliques du Joker de Ledger.
- *
- * On veut une variable qui représente le nombre de caractères max souhaité.
- * Et une autre pour les compter.
- *
- * On peut dépasser, car au final nous couperons et finirons par un '.' !
- */
-
 $replicas = array("Why so serious ?", "It's simple. We kill the Batman.", "Everything. Burns.", "This is my town now.", "The only sensible way to live in this world is without rules.");
-
-$count_carac = 0;
 $limit_carac = 1200;
+$nb_p = 5;
+$subsections = array();
 
-$nb_p = 0;
-$paragraph = array();
+for ($x = 0; $x < $nb_p; $x++) {
+    $paragraph = array();
+    $count_carac = 0;
 
-for ($x = 0; $x < $nb_p+1; $x++) {
     while ($count_carac < $limit_carac) {
         $rand = rand(0, count($replicas)-1);
         array_push($paragraph, $replicas[$rand]);
         $length = strlen(implode(",", $paragraph));
         $count_carac = $length;
     }
+
+    $paragraph = implode(",", $paragraph);
+    $paragraph = str_replace(",", " ", $paragraph);
+    $diff = ($limit_carac - strlen($paragraph) -1);
+    $paragraph = substr($paragraph, 0, $limit_carac);
+    $paragraph = rtrim($paragraph, ".? ");
+
+    array_push($subsections, $paragraph);
+    echo '<p>'.$subsections[$x].'.</p>';
 }
-
-echo 'COUNT_CARAC = '.$count_carac.' | LIMIT_CARAC = '.$limit_carac;
-
-$paragraph = implode(",", $paragraph);
-$paragraph = str_replace(",", " ", $paragraph);
-$diff = ($limit_carac - strlen($paragraph) -1);
-$paragraph = substr($paragraph, 0, $limit_carac);
-$paragraph = rtrim($paragraph, ".? ");
-echo '<p>'.$paragraph.'.</p>';
-
-echo strlen($paragraph);
-
-
-
-
